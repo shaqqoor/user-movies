50dd16f835adc7ce770947139150894e8c5e2263
Shaqqour <mshaqqourm@gmail.com>
2019-12-12 21:55:44 -0800

    Sets up the views and the controller for directors page

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -1,2 +1,13 @@
 class DirectorsController < ApplicationController
+
+    get '/directors' do
+        @directors = Director.all
+        erb :'directors/index'
+    end
+
+    get '/directors/:slug' do
+        @director = Director.find_by_slug(params[:slug])
+        erb :'directors/show'
+    end
+
 end
--- app/models/director.rb
+++ app/models/director.rb
@@ -1,3 +1,13 @@
 class Director < ActiveRecord::Base
     has_many :movies
+
+    def slug
+        self.name.split.map { |part| part.downcase }.join("-")
+    end
+
+    def self.find_by_slug(slug)
+        unslugifed = slug.split("-").map { |part| part.capitalize }.join(" ")
+        self.find_by_name(unslugifed)
+    end
+
 end
